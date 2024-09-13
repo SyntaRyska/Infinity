c56b028e502e70b8e3a7bf153a6a9d771c111dfd
SyntaRyska <nvryska@gmail.com>
2024-09-13 18:35:02 +0600

    starts

---
+++ src/main/java/kg/syntaryska/infinity/InfinityApplication.java
@@ -0,0 +1,13 @@
+package kg.syntaryska.infinity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfinityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfinityApplication.class, args);
+    }
+
+}
